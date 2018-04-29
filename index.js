56cbdb280c81626b635c9494c544768eceaff2ec
Kirk Chen <rwk0119@yahoo.com.tw>
2018-04-29 14:56:05 +0800

    Add ejs

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const path = require('path');
 const express = require('express');
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.get('/',(req, res)=>{
     res.send('Hello world!');
