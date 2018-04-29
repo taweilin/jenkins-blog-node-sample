47ad948ae2ce65e384e95930c21eb6acd31c4d31
Kirk Chen <rwk0119@yahoo.com.tw>
2018-04-29 14:42:33 +0800

    Integrate sequelize and create Post schema

---
+++ models/post.js
@@ -0,0 +1,11 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Post = sequelize.define('Post', {
+    title: DataTypes.STRING,
+    content: DataTypes.TEXT
+  }, {});
+  Post.associate = function(models) {
+    // associations can be defined here
+  };
+  return Post;
+};
