df9e867605e18fa4f2c1584ebc8f0def8486cb67
Kirk Chen <rwk0119@yahoo.com.tw>
2018-05-20 22:27:39 +0800

    Add post page

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
+var posts = require('./routes/posts.js');
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use('/posts', posts);
+
 app.get('/',(req, res)=>{
-    res.send('Hello world!');
+    res.render('index');
 })
 
 app.listen(3000);
---
+++ routes/posts.js
@@ -0,0 +1,24 @@
+var models = require('../models');
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res) {
+    models.Post
+        .findAll()
+        .then(function (posts) {
+            res.render('posts/index', {
+                posts: posts
+            });
+        });
+});
+
+router.post('/create', function (req, res) {
+    models.Post.create({
+        title: req.body.title,
+        content: req.body.content
+    }).then(function () {
+        res.redirect('/');
+    });
+});
+
+module.exports = router;
