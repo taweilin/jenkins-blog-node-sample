ef4cab7eb1d91014956943cf49366c19f9f026ec
Kirk Chen <rwk0119@yahoo.com.tw>
2018-07-09 08:55:34 +0800

    Add test commit

--- services/discountService.js
+++ services/discountService.js
@@ -1,6 +1,10 @@
 function DiscountService() {}
 
 DiscountService.prototype.getDiscount = function (price) {
+    let target = -5;
+    let num = 3;
+    target = -num;
+
     if (price > 500) {
         return price * 0.8;
     }
